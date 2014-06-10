98586dc2afc2678449a365edad27405bc8ab8974
Sean Li <lishang106@gmail.com>
2014-06-09 21:00:38 -0700

    Base template.

---
+++ client/views/home.js
@@ -0,0 +1,8 @@
+Template.home.rendered = function() {
+  // things to do when the template is finished rendering
+};
+
+Template.home.events({
+  //".my-button click": function() {',
+    // do something clever',
+});
