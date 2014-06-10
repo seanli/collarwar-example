0a9c1071143bf0156a8b6644dc91f0f63ea7e98e
Sean Li <lishang106@gmail.com>
2014-06-10 00:15:55 -0700

    Press enter to search.

--- client/views/home.js
+++ client/views/home.js
@@ -12,5 +12,12 @@ Template.home.events({
   'click #button-search-friends': function () {
     var term = $('#input-search-friends').val();
     searchClientFriends(term);
+  },
+  'submit #form-search-friends': function (e) {
+    var term = $('#input-search-friends').val();
+    searchClientFriends(term);
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
   }
 });
